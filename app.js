85c823fbbf9b1d2eb6198b7a359ff43758c42c9f
superdev <superdev999@outlook.com>
2017-04-28 12:07:34 +0200

    update 04-28-2017

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 //require our dependencies
-var express = require ('express');
-var path = require('path');
-var app = express ();
-var port = 8080;
+const express = require ('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express ();
+const port = 8080;
 
-//route our app
-var router  = require('./app/routes');
-app.use ('/', router);
+/**
+ * Controllers (route handlers).
+ */
+const userController = require('./controllers/user');
 
 /**
  * Express configuration.
  */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.get('/', userController.homeView);
+app.get('/about', userController.aboutView);
+app.post('/', userController.getTempToken);
 
 //set static files (css, images, etc) location
 app.use (express.static(__dirname + '/public'));
 
 //start the server
 app.listen (port, function () {
-	console.log('app started!');
+	// console.log('app started!');
 });
--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,75 @@
 /**
  * Get Home Page
  */
-exports.getLogin = (req, res) => {
-  if (req.user) {
-    return res.redirect('/');
-  }
+exports.homeView = (req, res) => {
   res.render('pages/index', {
-    title: 'Finance Bot'
+    title: 'Finance Bot', 
+    error: 'no'
   });
+};
+
+/**
+ * Get About Page
+ */
+exports.aboutView = (req, res) => {
+  res.render('pages/about', {
+    title: 'About Us'
+  });
+};
+
+/**
+ * POST /
+ * Post Search Term.
+ */
+exports.getTempToken = (req, res, next) => {
+	
+	
+	client_id = req.body.id2;
+	client_secret = req.body.password;
+
+	// console.log("client_id : " + client_id);
+	// console.log("client_secret : " + client_secret);
+	
+	var Client = require('node-rest-client').Client;
+	var client = new Client();
+
+	var auth_token = "init";
+	var access_token = "init";
+
+	var args1 = {
+		data: {			
+			test: "hello"
+		},
+		headers: {"Content-Type": "application/x-www-form-urlencoded"}
+	};
+
+	client.post("https://tchokin.biapi.pro/2.0/auth/init", args1, function (data, response) {		
+		auth_token = data.auth_token;
+		console.log("auth_token: " + auth_token);
+
+		var args2 = {
+			data: {
+				client_id: client_id,
+				client_secret: client_secret,
+				code: auth_token
+			},
+			headers: {"Content-Type": "application/x-www-form-urlencoded"}
+		};
+
+		client.post("https://tchokin.biapi.pro/2.0/auth/token/access", args2, function (data, response) {
+			if(data.error != undefined) {
+				console.log("Invalid Client! Please recheck your credential!");
+				return res.render('pages/index', {
+				   title: 'Finance Bot', 
+				   error: data.error
+				});
+			}
+			console.log("access_token: " + data.access_token);
+			res.render('pages/processToken', {
+				title: 'Access Token',
+				access_token: data.access_token
+			});
+		});
+
+	});
 };
