410bb7a4d900b64baefcaea76e692ac310d6cdea
superdev <superdev999@outlook.com>
2017-04-29 12:21:29 +0200

    update 04-29-2017

--- controllers/user.js
+++ controllers/user.js
@@ -45,7 +45,7 @@ exports.getTempToken = (req, res, next) => {
 
 	client.post("https://tchokin.biapi.pro/2.0/auth/init", args1, function (data, response) {		
 		auth_token = data.auth_token;
-		console.log("auth_token: " + auth_token);
+		//console.log("auth_token: " + auth_token);
 
 		var args2 = {
 			data: {
@@ -56,19 +56,30 @@ exports.getTempToken = (req, res, next) => {
 			headers: {"Content-Type": "application/x-www-form-urlencoded"}
 		};
 
-		client.post("https://tchokin.biapi.pro/2.0/auth/token/access", args2, function (data, response) {
-			if(data.error != undefined) {
+		client.post("https://tchokin.biapi.pro/2.0/auth/token/access", args2, function (data1, response) {
+			if(data1.error != undefined) {
 				console.log("Invalid Client! Please recheck your credential!");
 				return res.render('pages/index', {
 				   title: 'Finance Bot', 
-				   error: data.error
+				   error: data1.error
 				});
 			}
-			console.log("access_token: " + data.access_token);
-			res.render('pages/processToken', {
-				title: 'Access Token',
-				access_token: data.access_token
+
+			var args3 = {
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded",
+					"Authorization": "Bearer 5edIL7xUk/kYgAR9+tAF1AX/wuUbySCSZ3KCXpuf4yp0yUKJRjrlvKp6TzEojcRtZqn5Wr+mJqylsmEaZaRlXBC/f5zldjf4HpKNb26NqGjcyAewK2Sapy1y6DPDPOm7"
+				}
+			};
+
+			client.get("https://tchokin.biapi.pro/2.0/users/me/connections/47/accounts", args3, function(data2, response) {
+				console.log(data2);
+				res.render('pages/processToken', {
+					title: 'Bank Data',
+					bankdata: JSON.stringify(data2)
+				});
 			});
+			
 		});
 
 	});
